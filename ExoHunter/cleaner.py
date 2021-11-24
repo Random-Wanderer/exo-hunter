978e6d729a5a6943e4985f5ddc2567199b0bce51
Lorcan Odufuwa-Bolger <lorcanodufuwabolger@gmail.com>
2021-11-24 11:19:20 +0900

    updated cleaner to reflect train_test_val  splits

--- ExoHunter/cleaner.py
+++ ExoHunter/cleaner.py
@@ -11,12 +11,10 @@ class Cleaner():
     def __init__(self) -> None:
         pass
 
-    def get_data(self, data_name, set_='train', test_size=0.2):
-        '''Function that returns train , val, and test dataframes
-        for a given data path'''
-        path = os.path.join('raw_data', data_name, FILEPATHS[data_name][TRAIN_OR_TEST[set_]])
-        data = pd.read_csv(path)
-        if set_=='train':
-            data_train, data_val = train_test_split(data, test_size=test_size)
-            return data_train, data_val
-        return data
+    def get_data(self, data_name, test_size=0.2):
+        train_path = os.path.join('raw_data', data_name, FILEPATHS[data_name][TRAIN_OR_TEST["train"]])
+        test_path = os.path.join('raw_data', data_name, FILEPATHS[data_name][TRAIN_OR_TEST["test"]])
+        train_data = pd.read_csv(train_path)
+        data_test = pd.read_csv(test_path)
+        data_train, data_val = train_test_split(train_data, test_size=test_size)
+        return data_train, data_val, data_test
