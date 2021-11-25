c62de187f4bfb1c6fbcf08d670130b679ffc211e
Lorcan Odufuwa-Bolger <lorcanodufuwabolger@gmail.com>
2021-11-25 10:45:19 +0900

    completed data ingestion module

--- ExoHunter/params.py
+++ ExoHunter/params.py
@@ -1,7 +1,7 @@
 CONFUSION_COLS = ['Predicted 0', 'Predicted 1']
 CONFUSION_INDS = ['Actual 0', 'Actual 1']
 FILEPATHS = {
-    'kaggle': ['exoTrain.csv', 'exoTest.csv'],
+    'kaggle': [['exoTrain.csv', 'exoTest.csv'], ['exoTrain.csv', 'exoTest.csv']],
     'nasa': [['nasa_exo_wo_label_df.csv', 'nasa_non_exo_wo_label_df.csv'], ['nasaTrain.csv', 'nasaTest.csv']]
 }
 TRAIN_OR_TEST = {
@@ -9,6 +9,6 @@ TRAIN_OR_TEST = {
     'test': 1
 }
 DRIVE = {
-    0: 'raw_data',
-    1: '/content/drive/MyDrive/Colab Notebooks/Awle/exo-hunter/raw_data'
+    0: '..',
+    1: '/content/drive/MyDrive/Colab Notebooks/Awle/exo-hunter'
 }
